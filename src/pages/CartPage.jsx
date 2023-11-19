0a6e865bb6dc4e12a9e5c9c31d40a8bd7bdcda0b
Rezzoug Yannis <rezzoug.yannis@gmail.com>
2023-11-19 11:19:43 +0100

    edit navbar

---
+++ src/pages/CartPage.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {useDispatch, useSelector} from "react-redux";
+import {getData} from "../Features/products/productsService";
+import Cart from '../Components/Cart/Cart'
+
+
+
+function CartPage(props) {
+    const productState = useSelector(state => state.products);
+    const dispatch = useDispatch();
+    const products = productState.data
+
+
+    React.useEffect(() => {
+        dispatch(getData());
+    },[dispatch])
+
+    return (
+      <div className="container">
+        <Cart products={products}/>
+      </div>
+    );
+}
+
+export default CartPage;
